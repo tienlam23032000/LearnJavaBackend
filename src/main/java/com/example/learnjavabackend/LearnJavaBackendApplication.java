85db6b7d0cd0de27d7b23d2ac527910982d06a63
tienlam23032000 <tienlam23032000@gmail.com>
2023-10-05 21:22:45 +0700

    tl hoc code java

---
+++ src/main/java/com/example/learnjavabackend/LearnJavaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.learnjavabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnJavaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnJavaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/learnjavabackend/LearnJavaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learnjavabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnJavaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
